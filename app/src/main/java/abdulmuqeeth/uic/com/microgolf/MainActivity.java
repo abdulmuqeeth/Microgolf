6c04c8ff17aa7134832d46866d6a382b8e2817e1
abdulmuqeethmohammed <amoham62@uic.edu>
2018-04-11 16:55:26 -0500

    Initial commit

---
+++ app/src/main/java/abdulmuqeeth/uic/com/microgolf/MainActivity.java
@@ -0,0 +1,13 @@
+package abdulmuqeeth.uic.com.microgolf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
